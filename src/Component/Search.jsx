afe8b0c07af7db89c8736cf62b7ebc77e8df9131
itsShubhamShinde <itsshubhamks@gmai.com>
2023-03-14 19:00:23 +0530

    update

--- src/Component/Search.jsx
+++ src/Component/Search.jsx
@@ -4,7 +4,7 @@ import './Search.css'
 const Search = ({ inputvalue, setInputvalue, value, setValue }) => {
 
     return (
-        <div className='Search' >
+        <div className='Search' style={{position:"sticky",top:"0" , left:"0"}}>
             <div className='name'> 
                 <h2>Movie Search App</h2>
             </div>
